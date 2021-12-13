f8f8669960e4b48cc6d740ec23e68e6e93b58179
MarcRobichaud <marc_robi@yahoo.ca>
2021-12-13 13:16:51 -0500

    player class

---
+++ Assets/Resources/Scripts/Player/Player.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private HashSet<Unit> Mobs;
+    private HashSet<Unit> Towers;
+}
--- Assets/Resources/Scripts/Units/Unit.cs
+++ Assets/Resources/Scripts/Units/Unit.cs
@@ -1,7 +1,26 @@
 using UnityEngine;
+using FSM;
 
 public class Unit : MonoBehaviour, IAttacker, IHitable
 {
+    private Moveable moveable;
+    private StateMachine fsm;
+
+    private void Awake()
+    {
+        moveable = GetComponent<Moveable>();
+    }
+
+    private void Start()
+    {
+        fsm = new StateMachine();
+
+        fsm.AddState("MoveToTower",
+            onEnter:
+
+            )
+    }
+
     public void OnHit(int damage)
     {
     }
